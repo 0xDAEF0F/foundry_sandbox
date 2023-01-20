7085125b9adfa4cf87687a6a0bd23f7891e325dc
Alex T <aletapia@proton.me>
2023-01-20 10:56:29 -0600

    added yul examples

--- src/YulExample.sol
+++ src/YulExample.sol
@@ -20,4 +20,55 @@ contract YulExample {
             a := 0x1
         }
     }
+
+    // returns an address 0x000...1
+    function whatIsTheMeaningOfLifeC() external pure returns (address a) {
+        assembly {
+            a := 0x1
+        }
+    }
+
+    function isPrime(uint256 x) public pure returns (bool p) {
+        p = true;
+        assembly {
+            // declare new variable
+            let halfX := add(div(x, 2), 1)
+            for { let i := 2 } lt(i, halfX) { i := add(i, 1) } {
+                // ^Ini        ^Condition   ^Afterthought
+                // OPTIONAL    NECESSARY    OPTIONAL
+                // OPTIONAL BLOCKS NEED THE BRACKETS TO COMPILE
+                if iszero(mod(x, i)) {
+                    p := 0
+                    break
+                }
+            }
+        }
+    }
+
+    // 0 evaluates to false the result to true
+    function isTruthy(uint256 _val) external pure returns (bool result) {
+        assembly {
+            if _val { result := 1 }
+        }
+    }
+
+    function negation(uint256 _num) external pure returns (uint256 result) {
+        assembly {
+            result := 1
+            if iszero(_num) { result := 2 }
+        }
+    }
+
+    function bitFlip(uint256 _value) external pure returns (bytes32 result) {
+        assembly {
+            result := not(_value)
+        }
+    }
+
+    function max(uint256 x, uint256 y) external pure returns (uint256 _max) {
+        assembly {
+            _max := x
+            if lt(x, y) { _max := y }
+        }
+    }
 }
---
+++ src/YulStorage.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Slot is where the actual variable is (var.slot gives the location)
+// sload(slot)
+// sstore(slot, value)
+contract YulStorage {
+    uint256 private a;
+    uint256 private x;
+
+    function getXSlot() external pure returns (uint256 _x) {
+        assembly {
+            _x := x.slot
+        }
+    }
+
+    function getXYul() external view returns (uint256 _x) {
+        assembly {
+            // _x := sload(x.slot) EQUIVALENT
+            _x := sload(1)
+        }
+    }
+
+    function setXYul(uint256 _newX) external {
+        assembly {
+            // takes the slot and the value
+            sstore(1, _newX)
+        }
+    }
+}
