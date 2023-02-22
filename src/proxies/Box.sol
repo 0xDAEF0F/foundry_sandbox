5834ef561c3710b48c0c53a94013d34fd8c7a8ec
daemon <0xDAEF0F@proton.me>
2023-02-22 16:10:08 -0600

    forge install: openzeppelin-contracts
    v4.8.1

--- src/proxies/Box.sol
+++ src/proxies/Box.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.18;
 
-// import {} from "@openzeppelin/";
+import {Initializable} from "@openzeppelin/proxy/utils/Initializable.sol";
 
 contract Box {}
