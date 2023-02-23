2c33181842a368ad1877bdf7be3ed7208fa6ec57
daemon <0xDAEF0F@proton.me>
2023-02-23 11:14:04 -0600

    thu feb 23

--- src/proxies/Box.sol
+++ src/proxies/Box.sol
@@ -1,6 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.18;
 
-import {Initializable} from "@openzeppelin/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
+import {Initializable} from "openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
 
 contract Box {}
+
+contract Logic is Initializable {
+    uint256 public x;
+
+    function initialize(uint256 _x) public initializer {
+        x = _x;
+    }
+}
